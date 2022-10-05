62cb7c7824fc1b876dd1a7748f528b77110b0e04
Ludmila Dias <ludmiladias.inf@gmail.com>
2022-10-05 14:39:25 -0300

    Dog Scrapping v3

--- dogScraping.js
+++ dogScraping.js
@@ -31,8 +31,13 @@ async function start(){
 
     criarPastas(nomes);
     await acessarLinks(links, nomes, page).catch(e => { console.log(e) });
-    await browser.close().catch(e => { console.log(e) });
+    await browser.close();
 }
+
+// -----------------------------------------------------------------------------------------------------------------------
+// Chama a função principal
+start().catch(e => { console.log(e) });
+
 // ------------------------------------------------------------------------------------------------------
 // Funções utilizadas
 
@@ -62,17 +67,6 @@ function criarPastas(nomes){
     }
 }
 
-// //  Baixa as imagens da lista imagens
-// async function baixarImagens(imagens, nome, page){
-//     let cod = 0
-//     for(const imagem of imagens){
-//         const pagina = await page.goto(imagem);
-//         await page.setDefaultNavigationTimeout(0); 
-
-//         await fsp.writeFile(`./Dogs/${nome}/${cod}.${imagem.split("/").pop()}`, await pagina.buffer())
-//         cod ++;
-//     }
-// }
 
 // Acessa links que direcionam pro site de cada raça
 async function acessarLinks(links, nomes, page){
@@ -83,7 +77,6 @@ async function acessarLinks(links, nomes, page){
         if (!(fs.readdirSync(`./Dogs/${nomes[j]}`).length)){
             console.log("entrou")
             await page.goto(link).catch(e => { console.log(e) });
-            page.waitForNavigation()
 
             const imagens = await page.evaluate(() => {
                 const lista = [];
@@ -111,20 +104,4 @@ async function acessarLinks(links, nomes, page){
         j ++;
     }
 }
-// -----------------------------------------------------------------------------------------------------------------------
-// Chama a função principal
-start().catch(e => { console.log(e) });
 
-
-/* ERRO NA SAIDA APÓS BAIXAR TODOS AS IMAGENS:
-(node:12032) UnhandledPromiseRejectionWarning: Error: Navigation failed because browser has disconnected!
-    at new LifecycleWatcher (c:\Users\Usuario\node_modules\puppeteer\lib\cjs\puppeteer\common\LifecycleWatcher.js:95:223)
-    at Frame.waitForNavigation (c:\Users\Usuario\node_modules\puppeteer\lib\cjs\puppeteer\common\Frame.js:261:25)
-    at Page.waitForNavigation (c:\Users\Usuario\node_modules\puppeteer\lib\cjs\puppeteer\common\Page.js:1218:88)
-    at acessarLinks (c:\Users\Usuario\Desktop\PROJETOS\scraper\dogScraping.js:86:18)
-    at processTicksAndRejections (internal/process/task_queues.js:95:5)
-    at async start (c:\Users\Usuario\Desktop\PROJETOS\scraper\dogScraping.js:33:5)
-(Use `node --trace-warnings ...` to show where the warning was created)
-(node:12032) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
-(node:12032) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
-*/
