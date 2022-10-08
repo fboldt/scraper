16ca1b9e1637d9131e6b82c21a7b1304379f7b0b
Francisco de Assis Boldt <fboldt@gmail.com>
2022-10-08 10:37:14 -0300

    change native node to express

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end("Database Scraper\nhttps://github.com/fboldt/scraper");
-});
+const express = require('express')
+const app = express()
+app.use(express.static('./public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 const port = process.env.PORT || 3000
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
-});
+const start = async () => {
+    try {
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+start()
