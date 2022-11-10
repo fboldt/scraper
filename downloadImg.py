49504a63eec220e2897f5e3a8aca6c7f4727f282
Ludmila Dias <ludmiladias.inf@gmail.com>
2022-11-10 13:12:27 -0300

    new updates at download py file

--- downloadImg.py
+++ downloadImg.py
@@ -7,11 +7,34 @@ import os
 
 ########################################
 
+def baixarImgs(dados):
+    for item in dados:
+        for raca, links in item.items():
+            
+            dir = './racas/'+ raca
+            
+            if not(os.path.isdir(dir)):
+                os.mkdir(dir)
+            if (len(os.listdir(dir)) == 0):
+                for link in links:
+                    nomeArq = dir+"/" + link.split("/").pop()
+                    r = requests.get(link, stream=True)
+                    with open(nomeArq, 'wb') as f:
+                        f.write(r.content)
+                    
+            else: 
+                for link in links:
+                    nomeArq = dir+"/" + link.split("/").pop()
+                    if not(os.path.isfile(nomeArq)):
+                        r = requests.get(link, stream=True)
+                        with open(nomeArq, 'wb') as f:
+                            f.write(r.content)
+                        
+                        
+            print(f"Raca: {raca}\nImagens: {links}\nTotal de Imagens: {len(links)}\n")
 
-def pegarDados():
 
-  link = "http://127.0.0.1:3000/"
-
+def main():
 
   with open('public/lista.json', encoding='utf-8') as meu_json:
     dados = json.load(meu_json)
@@ -20,27 +43,11 @@ def pegarDados():
     os.mkdir('./racas')
 
   print(dados)
-  for item in dados:
-    for raca, links in item.items():
-        print(f"{raca},{links}\n")
-        dir = './racas/'+raca
-        if not(os.path.isdir(dir)):
-            os.mkdir(dir)
-        if (len(os.listdir(dir)) == 0):
-            for link in links:
-                r = requests.get(link, stream=True)
-                with open(dir+"/" + link.split("/").pop(), 'wb') as f:
-                    f.write(r.content)
-        else: 
-            for link in links:
-                nomeArq = dir+"/" + link.split("/").pop()
-                if not(os.path.isfile(nomeArq)):
-                    r = requests.get(link, stream=True)
-                    with open(nomeArq, 'wb') as f:
-                        f.write(r.content)
+  baixarImgs(dados)
     
-    meu_json.close()
+  print(f'Total de racas: {len(dados)}')
+  meu_json.close()
 
 
 if __name__ == '__main__':
-  pegarDados()
+    main()
