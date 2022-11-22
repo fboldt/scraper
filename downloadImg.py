bc256cdf7eeeb55feb36035227c8a05384d530f0
engeneland <engenelandbr@gmail.com>
2022-11-22 11:19:24 -0300

    new updates

--- downloadImg.py
+++ downloadImg.py
@@ -1,15 +1,21 @@
-from flask import Flask, jsonify
+
 
 import json
 import requests
 
 import os
+import sys
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 ########################################
 
 def baixarImgs(dados):
+    lstError = {}
     for item in dados:
         for raca, links in item.items():
+            lst = []
             
             dir = './racas/'+ raca
             
@@ -19,19 +25,25 @@ def baixarImgs(dados):
                 for link in links:
                     nomeArq = dir+"/" + link.split("/").pop()
                     r = requests.get(link, stream=True)
-                    with open(nomeArq, 'wb') as f:
-                        f.write(r.content)
-                    
+                    try:
+                        with open(nomeArq, 'wb') as f:
+                            f.write(r.content)
+                    except:
+                        lst.append(nomeArq)
             else: 
                 for link in links:
                     nomeArq = dir+"/" + link.split("/").pop()
                     if not(os.path.isfile(nomeArq)):
                         r = requests.get(link, stream=True)
-                        with open(nomeArq, 'wb') as f:
-                            f.write(r.content)
+                        try:
+                            with open(nomeArq, 'wb') as f:
+                                f.write(r.content)
+                        except:
+                            lst.append(nomeArq)
+                            
+            lstError[raca] = lst            
                         
-                        
-            print(f"Raca: {raca}\nImagens: {links}\nTotal de Imagens: {len(links)}\n")
+            print(f"Raca: {raca} \nImagens: {links}\nTotal de Imagens: {len(links)}\n")
 
 
 def main():
@@ -42,9 +54,10 @@ def main():
   if not(os.path.isdir('racas')):
     os.mkdir('./racas')
 
-  print(dados)
-  baixarImgs(dados)
-    
+  racasSemLink = dados.pop(0)
+  imgsNBaixadas = baixarImgs(dados)
+  
+  print(racasSemLink)
   print(f'Total de racas: {len(dados)}')
   meu_json.close()
 
--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 async function geraJson(url, quantidade) {
     const browser = await puppeteer.launch();
@@ -9,48 +10,56 @@ async function geraJson(url, quantidade) {
     const retorno = await selecionaImagensPaginas(page, quantidade)
     browser.close()
     await fs.writeFile("public/lista.json", JSON.stringify(retorno))
-    const link = "<a href='lista.json'>donwload</a>"
+    const link = "<a href='lista.json'>download</a>"
     return link
 };
 
 async function selecionaImagensPaginas(page, quantidade) {
-    retorno = []
+    const retorno = []
+    var json_aux = {}
+    json_aux["Racas Sem Links"]= await nomeSemLink(page)
+    retorno.push(json_aux)
+
     const links = await listaLinks(page)
     const nomes = await listaNomes(page)
+    
 
     if (quantidade == 0) quantidade = nomes.length
-
     //acessa todos os links e seleciona as imagens de cada página
     for (let m = 0; m < quantidade; m++) {
         const json_aux = {}
         await page.goto(links[m]);
         page.waitForNavigation()
         const imagens = await avaliaPagina(page)
-        tituloPag = await page.evaluate(async () => {
-            return document.querySelector('#firstHeading > span').textContent;
-        });
-        console.log(tituloPag + " pop")
-        while(tituloPag !== nomes[m]){
-            json_aux[nomes[m]] = [];
-            retorno.push(json_aux)
-            m++
-        }
-        json_aux[nomes[m]] = imagens;
         console.log(nomes[m])
+        json_aux[nomes[m]] = imagens;
         retorno.push(json_aux)
     }
     return JSON.parse(JSON.stringify(retorno))
 }
 
 async function listaLinks(page) {
-    return await page.evaluate(() => {
+    return page.evaluate(() => {
         return Array.from(document.querySelectorAll(".div-col ul li > a")).map(n => n.href)
     })
 }
 
 async function listaNomes(page) {
-    return await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".div-col ul li > a")).map(n => n.title)
+    return page.evaluate(() => {
+        return list = Array.from(document.querySelectorAll(".div-col ul li > a")).map(n => n.title)
+    })
+}
+
+async function nomeSemLink(page){
+    return page.evaluate(() => {
+        const lista = Array.from(document.querySelectorAll(".div-col ul li")).map(n => n.firstChild.data)
+        const newList = Array.from(document.querySelectorAll(".div-col ul li > span")).map(n => n.id)
+        for (item of lista){
+            if (item != null){
+                newList.push(item)
+            }
+        }      
+        return newList
     })
 }
 
